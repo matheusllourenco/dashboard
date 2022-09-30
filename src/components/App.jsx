5a576ff23a10e978a97eebfd17033ef88ccbed03
tsunodajapa <gabrieltsunoda@gmail.com>
2022-09-29 21:23:53 -0300

    feat: notfound

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import SideBar from "./SideBar";
 import { Switch, Route } from "react-router-dom";
 import ContentWrapper from "./ContentWrapper";
+import Genres from "./GenresInDb";
+import LastMovieInDb from "./LastMovieInDb";
+import ContentRowMovies from "./ContentRowMovies";
+import { NotFound } from "./NotFound";
 
 function App() {
   return (
@@ -12,9 +16,16 @@ function App() {
         <Route exact path="/">
           <ContentWrapper />
         </Route>
-        <Route path="/dashboard">
-          <h1>Teste</h1>
+        <Route path="/genres">
+          <Genres />
         </Route>
+        <Route path="/last-movie">
+          <LastMovieInDb />
+        </Route>
+        <Route path="/content-movies">
+          <ContentRowMovies />
+        </Route>
+        <Route component={NotFound} />
       </Switch>
     </div>
   );
